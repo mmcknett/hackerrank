8323bcfac19a60d512f4613fb76264d94971d66e
Matt McKnett <mmcknett@gmail.com>
2017-06-06 00:08:48 -0700

    Create files for maximum_draws

---
+++ maximum_draws/maximum_draws.cpp
@@ -0,0 +1,18 @@
+//
+// Problem statement: https://www.hackerrank.com/challenges/maximum-draws
+//
+// Copyright 2017 Matt McKnett
+//
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
